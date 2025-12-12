f86fc1735fad4a2042d48a20b953c6484966d9db
D4niel-Diaz <danieldoiesta00@gmail.com>
2025-12-12 15:49:10 +0800

    Web display fix and remove unecessaries data

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Welcome bro bro0';
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-?>
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Inventory Management System API',
+        'version' => '1.0.0',
+        'status' => 'running'
+    ]);
+});
